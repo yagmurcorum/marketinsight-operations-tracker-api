d403ed16f707a5e99fe58991fda935ea984b745e
Yağmur Çorum <corumyagmur@hotmail.com>
2026-05-23 19:43:50 +0300

    feat: add system info endpoint

---
+++ src/MarketInsight.Api/Controllers/SystemController.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MarketInsight.Api.Controllers;
+
+/// <summary>
+///  Provides basic system information about the MarketInsight Operations Tracker API.
+/// </summary>
+[ApiController]
+[Route("api/system")]
+public class SystemController : ControllerBase
+{
+    private readonly IWebHostEnvironment _environment;
+
+    public SystemController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+    /// <summary>
+    /// Returns basic application name, version and environment information.
+    /// </summary>
+    /// <returns>Basic system information about the API.</returns>
+
+    [HttpGet("info")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+
+    public IActionResult GetSystemInfo() 
+
+    {
+        var response = new
+        {
+            applicationName = "MarketInsight Operations Tracker API",
+            version = "1.0.0",
+            environment = _environment.EnvironmentName
+        };
+    
+        return Ok(response);
+    }
+
+}
